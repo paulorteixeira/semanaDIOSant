cbf758a9fc45d1695e751e6bc30383ae411ec33e
Paulo Ricardo <1981928paulo@gmail.com>
2023-10-20 15:55:22 -0300

    Publicando Railway

---
+++ src/main/java/com/example/demo/domain/controller/UserController.java
@@ -0,0 +1,45 @@
+package com.example.demo.domain.controller;
+
+import java.net.URI;
+
+import org.apache.catalina.connector.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.demo.domain.model.User;
+import com.example.demo.domain.service.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    
+    private final UserService userService;
+
+    public UserController(UserService userService){
+        this.userService = userService;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Long id){
+        var user = userService.findById(id);
+        return ResponseEntity.ok(user);
+    }
+    
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user){
+        var userCreated = userService.create(user);
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+        .path("/{id}")
+        .buildAndExpand(userCreated.getId()).toUri();
+        
+        return ResponseEntity.created(location).body(userCreated);
+    }
+
+    
+}
