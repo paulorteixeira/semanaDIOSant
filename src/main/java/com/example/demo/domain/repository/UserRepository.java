b27fedc6de5aabf4c1af6f233e9217a9135827fa
Paulo Ricardo <1981928paulo@gmail.com>
2023-10-20 10:42:11 -0300

    Implementação dominio

---
+++ src/main/java/com/example/demo/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.domain.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.domain.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User,Long>{
+    
+}
